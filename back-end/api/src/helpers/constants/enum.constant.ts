1a0bfd1f78c3838b32f4df1cfee720097fbecdb4
DuyCR7 <anhduy0317@gmail.com>
2024-12-10 17:06:11 +0700

    base models and auth, manage user

--- back-end/api/src/helpers/constants/enum.constant.ts
+++ back-end/api/src/helpers/constants/enum.constant.ts
@@ -47,3 +47,23 @@ export enum AppType {
   WEB_HOOK = 'WEB_HOOK',
   REPORT = 'REPORT',
 }
+
+export enum Transport {
+  TRANSPORT_FACILITY = 'Transport Facility',
+  PRIVATE_CAR = 'Private Car',
+  SHARED_BUS = 'Shared Bus',
+}
+
+export enum Package {
+  FAMILY_PLAN = 'Family Plan',
+  COUPLE_PLAN = 'Couple Plan',
+  SINGLE_PLAN = 'Single Plan',
+}
+
+export enum Duration {
+  ZERO_TO_THREE_HOURS = '0-3 hours',
+  THREE_TO_FIVE_HOURS = '3-5 hours',
+  FIVE_TO_SEVEN_HOURS = '5-7 hours',
+  FULL_DAY = 'Full day (7+ hours)',
+  MULTI_DAY = 'Multi-day'
+}
--- back-end/api/src/modules/user/user.module.ts
+++ back-end/api/src/modules/user/user.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { PrismaModule } from 'prisma/prisma.module';
 import { CsvService } from 'src/core/services/csv.service';
 import { I18nCustomModule } from 'src/resources/i18n/i18n.module';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { AuthModule } from 'src/core/auth/auth.module';
 
 @Module({
   controllers: [UserController],
@@ -12,6 +13,7 @@ import { UserService } from './user.service';
   imports: [
     I18nCustomModule,
     PrismaModule,
+    forwardRef(() => AuthModule),
   ],
 })
 export class UserModule { }
