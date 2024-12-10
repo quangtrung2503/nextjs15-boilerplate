48251c45080d4b485c72c3702085b382395ae764
Nguyen Duc Long <dlong.work@gmail.com>
2024-12-10 10:42:11 +0700

    update config layout

---
+++ front-end/src/app/[locale]/layout.tsx
@@ -0,0 +1,15 @@
+import DefaulLayout from "@/components/DefaultLayout";
+import React from "react";
+interface LayoutProps {
+    children: React.ReactNode;
+  }
+const Layout = (props: LayoutProps) => {
+    const {children} = props
+  return (
+    <DefaulLayout>
+      <div>{children}</div>
+    </DefaulLayout>
+  );
+};
+
+export default Layout;
