51e3c6b6bf7680269a1c819dad31f4e5ef3ac829
Nguyen Duc Long <dlong.work@gmail.com>
2024-12-12 17:43:21 +0700

    Update auth

--- front-end/src/assets/index.ts
+++ front-end/src/assets/index.ts
@@ -2,3 +2,22 @@ export {default as banner} from "./images/banner.jpg"
 export {default as alaska} from "./images/alaska.jpg" 
 export {default as bannerTrendingCity} from "./images/trendingCitybanner.jpg" 
 export {default as imagesTrendingCity} from "./images/imageTrendingCity.jpg" 
+
+export {default as VNFlag} from './images/VNFlag.jpg'
+export {default as UKFlag} from './images/UKFlag.png'
+// Payment methods
+export {default as american_express} from './images/payment_method/american_express.png'
+export {default as apple_pay} from './images/payment_method/apple_pay.png'
+export {default as bit_pay} from './images/payment_method/bit_pay.png'
+export {default as discover} from './images/payment_method/discover.png'
+export {default as gg_pay} from './images/payment_method/gg_pay.png'
+export {default as maestro} from './images/payment_method/maestro.png'
+export {default as mater_card} from './images/payment_method/mater_card.png'
+export {default as paypal} from './images/payment_method/paypal.png'
+export {default as sofort} from './images/payment_method/sofort.png'
+export {default as visa} from './images/payment_method/visa.png'
+
+// Backgrounds
+export {default as bgLogin} from './images/backgrounds/bg_login.jpg'
+
+
