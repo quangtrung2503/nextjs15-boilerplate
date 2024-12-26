3ca21f697c7ef1c5d41530ae358edec5f0613523
Xuan Ngoc <92130383+xuanngoc2k2@users.noreply.github.com>
2024-12-26 14:33:03 +0700

    update remove header and footer default layout in admin

--- front-end/src/components/DefaultLayout/index.tsx
+++ front-end/src/components/DefaultLayout/index.tsx
@@ -9,7 +9,7 @@ interface DefaultLayoutProps {
 }
 const DefaultLayout = (props: DefaultLayoutProps) => {
   const pathname = usePathname()
-  const hideHeaderFooter = pathname.includes("auth");
+  const hideHeaderFooter = pathname.includes("auth") || pathname.includes("admin");
   const { children } = props;
   return (
     <>
